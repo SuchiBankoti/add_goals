511a4826156c4b55c80db5c732d45800c1e75654
suchi <suchibankoti95@gmail.com>
2023-02-02 09:31:31 +0530

    blur add button

--- src/Form.js
+++ src/Form.js
@@ -3,20 +3,22 @@ import React, { useState } from "react";
 export default function Form(props) {
   const { addGoal } = props;
   const [formdata, setFormdata] = useState("");
+  function handleChange(e) {
+    setFormdata(e.target.value);
+  }
   return (
     <div className="form-container">
       <form className="form">
         <label>
           Course Goal
-          <input
-            type="text"
-            value={formdata}
-            onChange={(e) => setFormdata(e.target.value)}
-          ></input>
+          <input type="text" value={formdata} onChange={handleChange}></input>
         </label>
       </form>
       <br />
-      <button className="add-btn" onClick={() => addGoal(formdata)}>
+      <button
+        className={formdata ? "blur-btn" : "add-btn"}
+        onClick={() => addGoal(formdata)}
+      >
         Add Goal
       </button>
     </div>
